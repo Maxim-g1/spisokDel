3dc458eba647792b969b9ba279d8322bf7958482
Maxim-g1 <122662234+Maxim-g1@users.noreply.github.com>
2024-02-03 14:48:14 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+const myButton = document.querySelector('#myButton')
+const nameInp = document.querySelector('#nameInp')
+const list = document.querySelector('#list')
+
+
+myButton.addEventListener('click', () => {
+    //создать элемент
+
+    let newItem = document.createElement('li');
+
+    //Добавить элемент в список parent.appenChild(item)
+
+    list.appendChild(newItem)
+
+    // item.classList.add('') добавить элементу item класс
+
+    newItem.classList.add('item')
+
+    //записать содержимое поля ввода в элемент списка
+
+    newItem.textContent = nameInp.value
+    let delBtn = document.createElement('button');
+    delBtn.textContent = 'Удалить'
+    newItem.appendChild(delBtn)
+    // Очистить поле ввода
+    nameInp.value = ''
+    //Удаление элемента списка
+
+    delBtn.addEventListener('click', () => {
+        list.removeChild(newItem)
+
+    })
+})
